14ef1ee43a5d553ccd66e2460bc863cc0c5686fe
k3v1n888 <40829114+k3v1n888@users.noreply.github.com>
2020-05-07 00:44:24 +0800

    Add files via upload

---
+++ sync-data.sh
@@ -0,0 +1,6 @@
+export SLS_ENDPOINT=ap-southeast-3-intranet.log.aliyuncs.com
+export SLS_AK_ID=LTAI4G6go3Xp8C2hcQWcTjw2
+export SLS_AK_KEY=m9XyBTRXPW27KRiz74z2CK7HBAXnfd
+export SLS_PROJECT=my-actiontrail-001
+export SLS_LOGSTORE=actiontrail_all-actiontrain
+export SLS_CG="sycn_data"
